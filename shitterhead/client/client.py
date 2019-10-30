382135ad17d87b1aef774ac5f4d9fefdd0b13a46
Felix Daverio <felixdaverio99@gmail.com>
2019-10-30 22:17:41 +1100

    Add client files

--- shitterhead/client/client.py
+++ shitterhead/client/client.py
@@ -1,1 +1,23 @@
-ff
+#from network import Network
+# import pickle
+
+
+#def main():
+#	n = Network()
+#	player = n.send("hello")
+#	print(player)
+
+
+#main()
+
+import socket
+
+HOST = '192.168.0.4'  # The server's hostname or IP address
+PORT = 5555        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.connect((HOST, PORT))
+	while True:
+		s.sendall(b'Hello, world')
+		data = s.recv(1024)
+		print('Received', repr(data))
